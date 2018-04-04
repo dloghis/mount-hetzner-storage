3f13fd80273ff114ecc78dca8270c426eb9b1785
dloghis <dloghis@gmail.com>
2018-04-04 13:18:07 +0300

    new with time to wait

--- backup-mount.sh
+++ backup-mount.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-# set time in seconds 
-secs=$((2 * 3))
-
-while [ $secs -gt 0 ]; do
-   echo -ne "$secs\033[0K\r"
-   sleep 1
-   : $((secs--))
-done
-
-
-# Check if storage is mounted (be sure that your user and password are ok)
-if [ $(mount | grep 'YOUR-USER.your-storagebox.de:/servers-backup' | wc -l) -ne 1 ]
+if [ $(mount | grep 'YOUR-USER@YOUR-USER.your-storagebox.de:/servers-backup' | wc -l) -ne 1 ]
 then
-    echo 'YOUR-PASSWORD' | sshfs YOUR-USER.your-storagebox.de:/YUOR-FOLDER  /backup -o password_stdin,allow_other
+    echo 'YOUR-PASSWORD' | sshfs -o allow_other YOUR-USER@YOUR-USER.your-storagebox.de:/servers-backup /backup -o password_stdin,allow_other
+
+        # Allow your storage to mount if is busy
+        secs=$((2 * 2))
+        while [ $secs -gt 0 ]; do
+        echo -ne "$secs\033[0K\r"
+        sleep 1
+        : $((secs--))
+        done
+
     echo 'SSHFS has been mounted.'
 else
     echo 'SSHFS is already mounted.'
 fi
+
 
