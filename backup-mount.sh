ea0b79bba2a04ae82a5e5174e2185a03bf39774a
dloghis <dloghis@gmail.com>
2018-04-04 14:10:36 +0300

    777

--- backup-mount.sh
+++ backup-mount.sh
@@ -3,6 +3,8 @@
 # Check if your storage is mounted
 if [ $(mount | grep 'YOUR-USER@YOUR-USER.your-storagebox.de:/servers-backup' | wc -l) -ne 1 ]
 then
+
+    # umask=000 is for permitions 777
     echo 'YOUR-PASSWORD' | sshfs -o umask=000 YOUR-USER@YOUR-USER.your-storagebox.de:/servers-backup /backup -o password_stdin,allow_other
 
         # Allow your storage to mount if ιτ is busy
